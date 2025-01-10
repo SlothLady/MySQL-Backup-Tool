2796487470b28202d90e19679be6fbd1e93df513
Kate Davidson <kate.davidson@guest-services.co.uk>
2025-01-10 15:05:40 +0000

    Add local backup toggle, local backup deletion, logging and dbname-date-time file naming convention.

--- Database-Backup.sh
+++ Database-Backup.sh
@@ -5,69 +5,76 @@
 # Set connection settings below
 #
 # Options:
-#   -h            Display this message.
-#   -t            Dry-run; test connection settings and priveleges without backing up.
+#   -h -help      Display this message.
+#   -t -test      Dry-run; test connection settings and priveleges without backing up.
 #
 # Author: Kate Davidson - katedavidson.dev
 # Date 10/01/2025
-# Version 1.0
+# Version 1.1
 
 #~~~~~~~~Connection Settings~~~~~~~~#
 
-MYSQL_USERNAME=""
-MYSQL_PASSWORD=""
-MYSQL_DATABASE=""
-MYSQL_HOST="localhost"
-REMOTE_USER=""
-REMOTE_HOST=""
-REMOTE_PATH=""
-BACKUP_FILENAME="backup.sql.gz"
+MYSQL_USERNAME=""                # MySQL username
+MYSQL_PASSWORD=""                # MySQL password
+MYSQL_DATABASE=""                # MySQL database name
+MYSQL_HOST="localhost"           # MySQL hostname
+REMOTE_USER=""                   # Remote username
+REMOTE_HOST=""                   # Remote hostname
+REMOTE_PATH=""                   # Remote backup path
+BACKUP_PATH="./database-backups" # Directory where local backups should be made
+BACKUP_EXPIRES=-1                # Number of days after which local backups should be deleted, -1 for never
+DISABLE_LOCAL_BACKUPS=true       # Enable or disable storage and deletion of local backups, a temporary file will still be made
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 
 dry_run() {
     echo "Dry-run, not backing up."
     if [ -w "$(pwd)" ]; then
-        echo "Testing connection to remote host $REMOTE_HOST as user $REMOTE_USER"
-        ssh -o BatchMode=yes -o ConnectTimeout=5 "${REMOTE_USER}@${REMOTE_HOST}" 'echo "Login successful."' >/dev/null 2>&1
-        if [ $? -eq 0 ]; then
-            echo "Checking if remote path $REMOTE_PATH is writeable."
-            echo "Hello $REMOTE_HOST" | ssh -o BatchMode=yes -o ConnectTimeout=5 "${REMOTE_USER}@${REMOTE_HOST}" "REMOTE_PATH=${REMOTE_PATH} && cat > \${REMOTE_PATH}/test"
+        if [ -w "$BACKUP_PATH" ]; then
+            echo "Testing connection to remote host $REMOTE_HOST as user $REMOTE_USER"
+            ssh -o BatchMode=yes -o ConnectTimeout=5 "${REMOTE_USER}@${REMOTE_HOST}" 'echo "Login successful."' >/dev/null 2>&1
 
             if [ $? -eq 0 ]; then
-                echo "Testing connection to MySQL server $MYSQL_HOST as $MYSQL_USERNAME."
-                MYSQL_PWD="${MYSQL_PASSWORD}" mysql -u "${MYSQL_USERNAME}" -h "${MYSQL_HOST}" -e "SELECT 1;" >/dev/null 2>&1
+                echo "Checking if remote path $REMOTE_PATH is writeable."
+                echo "Hello $REMOTE_HOST" | ssh -o BatchMode=yes -o ConnectTimeout=5 "${REMOTE_USER}@${REMOTE_HOST}" "REMOTE_PATH=${REMOTE_PATH} && cat > \${REMOTE_PATH}/test"
 
                 if [ $? -eq 0 ]; then
-                    echo "Checking whether database $MYSQL_DATABASE can be used."
-                    RESULT=$(MYSQL_PWD="${MYSQL_PASSWORD}" mysql -u $MYSQL_USERNAME -h $MYSQL_HOST -e "USE $MYSQL_DATABASE;" >/dev/null 2>&1)
+                    echo "Testing connection to MySQL server $MYSQL_HOST as $MYSQL_USERNAME."
+                    MYSQL_PWD="${MYSQL_PASSWORD}" mysql -u "${MYSQL_USERNAME}" -h "${MYSQL_HOST}" -e "SELECT 1;" >/dev/null 2>&1
 
-                    if [[ $RESULT == *"ERROR"* ]]; then
-                        echo "Database is NOT usable, exiting." >&2
-                        exit 1
-                    else
-                        echo "Checking if user $MYSQL_USERNAME has PROCESS privileges."
-                        PRIVILEGES=$(MYSQL_PWD="${MYSQL_PASSWORD}" mysql -u $MYSQL_USERNAME -h $MYSQL_HOST -e "SHOW GRANTS FOR '$MYSQL_USERNAME'@'$MYSQL_HOST';" 2>/dev/null)
+                    if [ $? -eq 0 ]; then
+                        echo "Checking whether database $MYSQL_DATABASE can be used."
+                        RESULT=$(MYSQL_PWD="${MYSQL_PASSWORD}" mysql -u $MYSQL_USERNAME -h $MYSQL_HOST -e "USE $MYSQL_DATABASE;" >/dev/null 2>&1)
 
-                        if [[ $PRIVILEGES == *"PROCESS"* ]]; then
-                            echo "Connection test successful, exiting."
-                            exit 0
-                        else
-                            echo "User does NOT have PROCESS privilege, exiting." >&2
+                        if [[ $RESULT == *"ERROR"* ]]; then
+                            echo "Database is NOT usable, exiting." >&2
                             exit 1
+                        else
+                            echo "Checking if user $MYSQL_USERNAME has PROCESS privileges."
+                            PRIVILEGES=$(MYSQL_PWD="${MYSQL_PASSWORD}" mysql -u $MYSQL_USERNAME -h $MYSQL_HOST -e "SHOW GRANTS FOR '$MYSQL_USERNAME'@'$MYSQL_HOST';" 2>/dev/null)
+
+                            if [[ $PRIVILEGES == *"PROCESS"* ]]; then
+                                echo "Connection test successful, exiting."
+                                exit 0
+                            else
+                                echo "User does NOT have PROCESS privilege, exiting." >&2
+                                exit 1
+                            fi
                         fi
+                    else
+                        echo "Login to MySQL failed, exiting." >&2
+                        exit 1
                     fi
                 else
-                    echo "Login to MySQL failed, exiting." >&2
+                    echo "Writing to remote path failed, exiting." >&2
                     exit 1
                 fi
             else
-                echo "Writing to remote path failed, exiting." >&2
+                echo "Login to remote host or writing to remote path failed, exiting." >&2
                 exit 1
             fi
-
         else
-            echo "Login to remote host or writing to remote path failed, exiting." >&2
+            echo "The database backup directory is not writable, exiting." >&2
             exit 1
         fi
     else
@@ -78,68 +85,133 @@ dry_run() {
 
 live_run() {
     if [ -w "$(pwd)" ]; then
-        echo "Testing connection to remote host $REMOTE_HOST as user $REMOTE_USER"
-        ssh -o BatchMode=yes -o ConnectTimeout=5 "${REMOTE_USER}@${REMOTE_HOST}" 'echo "Login successful."' >/dev/null 2>&1
-        if [ $? -eq 0 ]; then
-            echo "Checking if remote path $REMOTE_PATH is writeable."
-            echo "Hello $REMOTE_HOST" | ssh -o BatchMode=yes -o ConnectTimeout=5 "${REMOTE_USER}@${REMOTE_HOST}" "REMOTE_PATH=${REMOTE_PATH} && cat > \${REMOTE_PATH}/test"
+        if [ -w "$BACKUP_PATH" ]; then
+            echo "Testing connection to remote host $REMOTE_HOST as user $REMOTE_USER"
+            ssh -o BatchMode=yes -o ConnectTimeout=5 "${REMOTE_USER}@${REMOTE_HOST}" 'echo "Login successful."' >/dev/null 2>&1
 
             if [ $? -eq 0 ]; then
-                echo "Testing connection to MySQL server $MYSQL_HOST as $MYSQL_USERNAME."
-                MYSQL_PWD="${MYSQL_PASSWORD}" mysql -u "${MYSQL_USERNAME}" -h "${MYSQL_HOST}" -e "SELECT 1;" >/dev/null 2>&1
+                echo "Checking if remote path $REMOTE_PATH is writeable."
+                echo "Hello $REMOTE_HOST" | ssh -o BatchMode=yes -o ConnectTimeout=5 "${REMOTE_USER}@${REMOTE_HOST}" "REMOTE_PATH=${REMOTE_PATH} && cat > \${REMOTE_PATH}/test"
 
                 if [ $? -eq 0 ]; then
-                    echo "Checking whether database $MYSQL_DATABASE can be used."
-                    RESULT=$(MYSQL_PWD="${MYSQL_PASSWORD}" mysql -u $MYSQL_USERNAME -h $MYSQL_HOST -e "USE $MYSQL_DATABASE;" >/dev/null 2>&1)
+                    echo "Testing connection to MySQL server $MYSQL_HOST as $MYSQL_USERNAME."
+                    MYSQL_PWD="${MYSQL_PASSWORD}" mysql -u "${MYSQL_USERNAME}" -h "${MYSQL_HOST}" -e "SELECT 1;" >/dev/null 2>&1
 
-                    if [[ $RESULT == *"ERROR"* ]]; then
-                        echo "Database is NOT usable, exiting." >&2
-                        exit 1
-                    else
-                        echo "Checking if user $MYSQL_USERNAME has PROCESS privileges."
-                        PRIVILEGES=$(MYSQL_PWD="${MYSQL_PASSWORD}" mysql -u $MYSQL_USERNAME -h $MYSQL_HOST -e "SHOW GRANTS FOR '$MYSQL_USERNAME'@'$MYSQL_HOST';" 2>/dev/null)
+                    if [ $? -eq 0 ]; then
+                        echo "Checking whether database $MYSQL_DATABASE can be used."
+                        RESULT=$(MYSQL_PWD="${MYSQL_PASSWORD}" mysql -u $MYSQL_USERNAME -h $MYSQL_HOST -e "USE $MYSQL_DATABASE;" >/dev/null 2>&1)
 
-                        if [[ $PRIVILEGES == *"PROCESS"* ]]; then
-                            echo "Dumping database $MYSQL_DATABASE to $BACKUP_FILENAME."
-                            MYSQL_PWD="${MYSQL_PASSWORD}" mysqldump -u "${MYSQL_USERNAME}" -h "${MYSQL_HOST}" --single-transaction --databases "${MYSQL_DATABASE}" 2>/dev/null | gzip -9 >$BACKUP_FILENAME
+                        if [[ $RESULT == *"ERROR"* ]]; then
+                            echo "Database is NOT usable, exiting." >&2
+                            exit 1
+                        else
+                            echo "Checking if user $MYSQL_USERNAME has PROCESS privileges."
+                            PRIVILEGES=$(MYSQL_PWD="${MYSQL_PASSWORD}" mysql -u $MYSQL_USERNAME -h $MYSQL_HOST -e "SHOW GRANTS FOR '$MYSQL_USERNAME'@'$MYSQL_HOST';" 2>/dev/null)
 
-                            if [ $? -eq 0 ]; then
-                                echo "Dumping database successful. Backing up to remote host $REMOTE_HOST as user $REMOTE_USER."
-                                scp $BACKUP_FILENAME $REMOTE_USER@$REMOTE_HOST:$REMOTE_PATH
+                            if [[ $PRIVILEGES == *"PROCESS"* ]]; then
+                                BACKUP_FILENAME="${MYSQL_DATABASE}_$(date +"%Y-%m-%d_%H-%M").sql.gz"
+                                echo "Dumping database $MYSQL_DATABASE to $BACKUP_PATH/$BACKUP_FILENAME."
+                                echo "BEGIN DUMP " $(date) >>logs-backup.log
+                                MYSQL_PWD="${MYSQL_PASSWORD}" mysqldump -u "${MYSQL_USERNAME}" -h "${MYSQL_HOST}" --single-transaction --databases "${MYSQL_DATABASE}" 2>/dev/null | gzip -9 >$BACKUP_PATH/$BACKUP_FILENAME
 
                                 if [ $? -eq 0 ]; then
-                                    echo "Backup to remote host completed successfully, exiting."
-                                    exit 0
+                                    echo "END DUMP " $(date) >>logs-backup.log
+                                    echo "Dumping database successful. Backing up to remote host $REMOTE_HOST as user $REMOTE_USER."
+                                    echo "BEGIN FILE TRANSFER " $(date) >>logs-backup.log
+                                    scp $BACKUP_PATH/$BACKUP_FILENAME $REMOTE_USER@$REMOTE_HOST:$REMOTE_PATH
+
+                                    if [ $? -eq 0 ]; then
+                                        echo "END FILE TRANSFER " $(date) >>logs-backup.log
+
+                                        if [ "$DISABLE_LOCAL_BACKUP" = true ]; then
+                                            echo "Backup to remote host successful, deleting temporary file."
+                                            echo "DELETING LOCAL BACKUP " $(date) >>logs-backup.log
+                                            rm $BACKUP_PATH/$BACKUP_FILENAME
+
+                                            if [ $? -eq 0 ]; then
+                                                echo "Local backup deleted, exiting."
+                                                echo "LOCAL BACKUP DELETED " $(date) >>logs-backup.log
+                                                echo "---------------------------------" >>logs-backup.log
+                                                exit 0
+                                            else
+                                                echo "Local backup deleting failed, exiting."
+                                                echo "DELETING LOCAL BACKUP FAILED " $(date) >>logs-backup.log
+                                                echo "---------------------------------" >>logs-backup.log
+                                                exit 1
+                                            fi
+                                        else
+                                            echo "Backup to remote host successful, exiting."
+                                            echo "---------------------------------" >>logs-backup.log
+                                            exit 0
+                                        fi
+                                    else
+                                        echo "Backup to remote host failed, exiting." >&2
+                                        echo "FILE TRANSFER FAILED " $(date) >>logs-backup.log
+                                        echo "---------------------------------" >>logs-backup.log
+                                        exit 1
+                                    fi
                                 else
-                                    echo "Backup to remote host failed, exiting." >&2
+                                    echo "Dumping database failed, exiting." >&2
+                                    echo "DUMP FAILED " $(date) >>logs-backup.log
+                                    echo "---------------------------------" >>logs-backup.log
                                     exit 1
                                 fi
                             else
-                                echo "Dumping database failed, exiting." >&2
+                                echo "User does NOT have PROCESS privilege, exiting." >&2
+                                echo "CONFIG ERROR " $(date) >>logs-backup.log
+                                echo "---------------------------------" >>logs-backup.log
                                 exit 1
                             fi
-                        else
-                            echo "User does NOT have PROCESS privilege, exiting." >&2
-                            exit 1
                         fi
+                    else
+                        echo "Login to MySQL failed, exiting." >&2
+                        echo "CONFIG ERROR " $(date) >>logs-backup.log
+                        echo "---------------------------------" >>logs-backup.log
+                        exit 1
                     fi
                 else
-                    echo "Login to MySQL failed, exiting." >&2
+                    echo "Writing to remote path failed, exiting." >&2
+                    echo "CONFIG ERROR " $(date) >>logs-backup.log
+                    echo "---------------------------------" >>logs-backup.log
                     exit 1
                 fi
+
             else
-                echo "Writing to remote path failed, exiting." >&2
+                echo "Login to remote host or writing to remote path failed, exiting." >&2
+                echo "CONFIG ERROR " $(date) >>logs-backup.log
+                echo "---------------------------------" >>logs-backup.log
                 exit 1
             fi
-
         else
-            echo "Login to remote host or writing to remote path failed, exiting." >&2
+            echo "The database backup directory is not writable, exiting." >&2
+            echo "CONFIG ERROR " $(date) >>logs-backup.log
+            echo "---------------------------------" >>logs-backup.log
             exit 1
         fi
     else
         echo "The current working directory is not writable, exiting." >&2
+        echo "CONFIG ERROR " $(date) >>logs-backup.log
+        echo "---------------------------------" >>logs-backup.log
         exit 1
     fi
+}
+
+date_diff() {
+    d1=$(date -d "$1" +%s)
+    d2=$(date -d "$2" +%s)
+    echo $(((d2 - d1) / 86400))
+}
+
+delete_backups() {
+    CURRENT_DATE=$(date +"%Y-%m-%d")
+    for FILE in "$BACKUP_PATH"/*.sql.gz; do
+        FILE_DATE=$(basename "$FILE" | cut -d'_' -f1)
+        AGE=$(date_diff "$FILE_DATE" "$CURRENT_DATE")
+        if [ "$AGE" -gt "$BACKUP_EXPIRES" ]; then
+            rm "$FILE"
+            echo "Deleted old backup $FILE"
+        fi
+    done
 }
 
 print_help() {
@@ -166,6 +238,10 @@ while [[ "$#" -gt 0 ]]; do
     esac
     shift
 done
+
 if [ "$#" -eq 0 ]; then
+    if [ "$BACKUP_EXPIRES" -ne -1 ] && [ "$DISABLE_LOCAL_BACKUPS" != "true" ]; then
+        delete_backups
+    fi
     live_run
 fi
--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # MySQL Backup Tool
 
-This script allows you to backup a MySQL database and upload the backup to a remote host. This script also has a dry run option to allow you to just test your connection settings and for neccessary privileges without doing a backup.
+This script allows you to backup a MySQL database to, optionally a local folder, and upload the backups to a remote server using SCP. This script also has a dry run option to allow you to just test your connection settings and for neccessary privileges without doing a backup.
 
 Can be automated with a cron job.
 
 ## Usage
 
-To use the script, run the following command:
+To use the script, use the following commands:
 
 ```bash
 bash Database-Backup.sh
@@ -30,14 +30,18 @@ nano Database-Backup.sh
 
 ```bash
 #~~~~~~~~Connection Settings~~~~~~~~#
-MYSQL_USERNAME=""
-MYSQL_PASSWORD=""
-MYSQL_DATABASE=""
-MYSQL_HOST="localhost"
-REMOTE_USER=""
-REMOTE_HOST=""
-REMOTE_PATH=""
-BACKUP_FILENAME="backup.sql.gz"
+
+MYSQL_USERNAME=""                # MySQL username
+MYSQL_PASSWORD=""                # MySQL password
+MYSQL_DATABASE=""                # MySQL database name
+MYSQL_HOST="localhost"           # MySQL hostname
+REMOTE_USER=""                   # Remote username
+REMOTE_HOST=""                   # Remote hostname
+REMOTE_PATH=""                   # Remote backup path
+BACKUP_PATH="./database-backups" # Directory where local backups should be made
+BACKUP_EXPIRES=-1                # Number of days after which local backups should be deleted, -1 for never
+DISABLE_LOCAL_BACKUPS=true       # Enable or disable storage and deletion of local backups, a temporary file will still be made
+
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 ```
 
@@ -58,12 +62,17 @@ Checking if user mysqluser has PROCESS privileges.
 Connection test successful, exiting.
 ```
 
-Regardless of whether you run the test or not, all of your configuration options, as well as working directory write permissions, MySQL priveleges and remote directory write permissions will be checked prior to doing a backup, this prevents a situation where you spend hours dumping a database only for the script to error due to lack of remote write permissions.
+Regardless of whether you run the test or not, all of your configuration options, as well as working directory, backup directory write permissions, MySQL priveleges and remote directory write permissions will be checked prior to doing a backup, this prevents a situation where you spend hours dumping a database only for the script to error due to lack of remote write permissions.
 
 ## Logging
 
-Currently this script does not create logs, however this feature is planned and there will be an option to store logs locally and remotely.
+Event logs are stored in the file ```logs-backup.log``` in the working directory of the script. Dry runs do not make log entries, write permissions are still checked for.
 
-## Backups by date
+## Naming Convention
 
-Currently this script does not name the remote backups with the date and time they were backed up however this is another planned feature and you will be able to specify the date and time format to be included in the file names.
+Backups will be named ````[MYSQL_DATABASE]YYYY-MM-DD_HH-MM.sql.gz````
+Where MYSQL_DATABASE is the name of the database being backed up, followed by the current date and time.
+
+## Deleting Old Backups
+
+By default, this script won't store local backups or delete old local backups, see ```BACKUP_EXPIRES=-1``` and ```DISABLE_LOCAL_BACKUPS``` in the configuration section, optionally backups can be stored locally as well as remotely and automatically deleted from the local backups folder after ```BACKUP_EXPIRES=x``` amount of days. Remote backups will never be deleted, this is by design.
