2ade3475caf012a7a7d0241795d68435ca7aca47
Kate Davidson <kate.davidson@guest-services.co.uk>
2025-01-13 14:36:39 +0000

    Temporarily disabled deleting backups

--- Database-Backup.sh
+++ Database-Backup.sh
@@ -206,23 +206,14 @@ date_diff() {
 
 delete_backups() {
     CURRENT_DATE=$(date +"%Y-%m-%d")
-    if ls "$BACKUP_PATH"/*.sql.gz 1> /dev/null 2>&1; then
-        for FILE in "$BACKUP_PATH"/*.sql.gz; do
-            FILE_DATE=$(basename "$FILE" | cut -d'_' -f2 | cut -d'.' -f1)
-            if [[ "$FILE_DATE" =~ ^[0-9]{4}-[0-9]{2}-[0-9]{2}_[0-9]{2}-[0-9]{2}$ ]]; then
-                FILE_DATE_ONLY=$(echo "$FILE_DATE" | cut -d'_' -f1)
-                AGE=$(date_diff "$FILE_DATE_ONLY" "$CURRENT_DATE")
-                if [ "$AGE" -gt "$BACKUP_EXPIRES" ]; then
-                    rm "$FILE"
-                    echo "Deleted expired backup $FILE."
-                fi
-            else
-                echo "File $FILE does not match the expected date and time format."
-            fi
-        done
-    else
-        echo "No expired backups to delete."
-    fi
+    for FILE in "$BACKUP_PATH"/*.sql.gz; do
+        FILE_DATE=$(basename "$FILE" | cut -d'_' -f1)
+        AGE=$(date_diff "$FILE_DATE" "$CURRENT_DATE")
+        if [ "$AGE" -gt "$BACKUP_EXPIRES" ]; then
+            rm "$FILE"
+            echo "Deleted old backup $FILE."
+        fi
+    done
 }
 
 print_help() {
@@ -300,8 +291,8 @@ for config_file in "${config_files[@]}"; do
                 fi
             else
                 if [ "$BACKUP_EXPIRES" -ne -1 ] && [ "$LOCAL_BACKUPS" = true ]; then
-                    echo "Checking for expired local backups."
-                    delete_backups
+                    #echo "Checking for expired local backups."
+                    #delete_backups
                 fi
                 live_run
                 if [ $? -ne 0 ]; then
