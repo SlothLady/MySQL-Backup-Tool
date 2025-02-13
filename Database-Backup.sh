706cc1e07a730d2de0f4a1f75e36dd1d8c904c83
Kate Davidson <kate.davidson@guest-services.co.uk>
2025-02-13 16:33:31 +0000

    Major codebase overhaul to simplify dry run and live run into one function, simplify logic and increase readability and editability.

--- Database-Backup.sh
+++ Database-Backup.sh
@@ -13,82 +13,27 @@
 # Date 24/01/2025
 
 version="1.4"
-config_path="$(dirname $(realpath $0))/conf.d"
 script_path="$(dirname $(realpath $0))"
+config_path="$script_path/conf.d"
+log_path="$script_path/logs-backup.log"
 
 slack_message() {
-    curl -X POST "$SLACK_WEBHOOK_URL" -H 'Content-Type: application/json' -d '{"attachments":[{"color":"'"$4"'","text": "*Log Message:*\n'"$1"'\n\n*Config File:*\n'"$2"'\n\n*Status:*\n'"$3"'\n\n*Hostname:*\n'"$(uname -n)"'"}]}' > /dev/null 2>&1
-}
-
-dry_run() {
-    echo "Dry-run, not backing up."
-    echo "Using config file $config_file"
-    if [ -w "$script_path" ]; then
-        if [ -w "$BACKUP_PATH" ]; then
-            echo "Testing connection to remote host $REMOTE_HOST as user $REMOTE_USER"
-            ssh -o BatchMode=yes -o ConnectTimeout=5 "${REMOTE_USER}@${REMOTE_HOST}" 'echo "Login successful."' >/dev/null 2>&1
-
-            if [ $? -eq 0 ]; then
-                echo "Checking if remote path $REMOTE_PATH is writeable."
-                echo "Hello $REMOTE_HOST" | ssh -o BatchMode=yes -o ConnectTimeout=5 "${REMOTE_USER}@${REMOTE_HOST}" "REMOTE_PATH=${REMOTE_PATH} && cat > \${REMOTE_PATH}/test"
-
-                if [ $? -eq 0 ]; then
-                    echo "Testing connection to MySQL server $MYSQL_HOST as $MYSQL_USERNAME."
-                    MYSQL_PWD="${MYSQL_PASSWORD}" mysql -u "${MYSQL_USERNAME}" -h "${MYSQL_HOST}" -e "SELECT 1;" >/dev/null 2>&1
-
-                    if [ $? -eq 0 ]; then
-                        echo "Checking whether database $MYSQL_DATABASE can be used."
-                        MYSQL_PWD="${MYSQL_PASSWORD}" mysql -u $MYSQL_USERNAME -h $MYSQL_HOST -e "USE $MYSQL_DATABASE;"  >/dev/null 2>&1
-
-                        if [ $? -eq 0 ]; then
-                            echo "Checking if user $MYSQL_USERNAME has required privileges."
-                            PRIVILEGES=$(MYSQL_PWD="${MYSQL_PASSWORD}" mysql -u $MYSQL_USERNAME -h $MYSQL_HOST -e "SHOW GRANTS FOR '$MYSQL_USERNAME'@'$MYSQL_HOST';" 2>/dev/null)
-
-                            if [[ $PRIVILEGES == *"SELECT"* && $PRIVILEGES == *"SHOW VIEW"* && $PRIVILEGES == *"TRIGGER"* && $PRIVILEGES == *"PROCESS"* ]]; then
-
-                                if [[ "$LOCAL_BACKUPS" = true ]]; then
-                                    echo "Local backups are enabled."
-
-                                    if [[ "$BACKUP_EXPIRES" -ne -1 ]]; then
-                                        echo "Local backups expire after $BACKUP_EXPIRES days."
-                                    else
-                                        echo "Local backups set to not expire."
-                                    fi
-                                fi
-                                echo "Connection test successful, exiting."
-                                return 0
-                            else
-                                echo "User does NOT have required privileges, exiting." >&2
-                                return 1
-                            fi
-                        else
-                            echo "Database is NOT usable, exiting." >&2
-                            return 1
-                        fi
-                    else
-                        echo "Login to MySQL failed, exiting." >&2
-                        return 1
-                    fi
-                else
-                    echo "Writing to remote path failed, exiting." >&2
-                    return 1
-                fi
-            else
-                echo "Login to remote host failed, exiting." >&2
-                return 1
-            fi
-        else
-            echo "The database backup directory is not writable, exiting." >&2
-            return 1
-        fi
-    else
-        echo "The script directory is not writable, exiting." >&2
-        return 1
+    if [ "$dry_run" = false ] && [ "$SLACK_INTEGRATION" = true ]; then
+        curl -X POST "$SLACK_WEBHOOK_URL" -H 'Content-Type: application/json' -d '{"attachments":[{"color":"'"$4"'","text": "*Log Message:*\n'"$1"'\n\n*Config File:*\n'"$2"'\n\n*Status:*\n'"$3"'\n\n*Hostname:*\n'"$(uname -n)"'"}]}' > /dev/null 2>&1
     fi
 }
 
-live_run() {
-    echo "BEGINS $config_file " $(date) >>$script_path/logs-backup.log
+log_message() {
+    if [ "$dry_run" = false ]; then
+        echo "$1 " $(date) >>$log_path
+    fi
+}
+
+run_backup() {
+    if [ "$dry_run" = true ]; then
+        echo "Dry-run, not backing up."
+    fi
+    log_message "BEGINS $config_file"
     echo "Using config file $config_file"
     if [ -w "$script_path" ]; then
         if [ -w "$BACKUP_PATH" ]; then
@@ -112,104 +57,125 @@ live_run() {
                             PRIVILEGES=$(MYSQL_PWD="${MYSQL_PASSWORD}" mysql -u $MYSQL_USERNAME -h $MYSQL_HOST -e "SHOW GRANTS FOR '$MYSQL_USERNAME'@'$MYSQL_HOST';" 2>/dev/null)
 
                             if [[ $PRIVILEGES == *"SELECT"* && $PRIVILEGES == *"SHOW VIEW"* && $PRIVILEGES == *"TRIGGER"* && $PRIVILEGES == *"PROCESS"* ]]; then
-                                BACKUP_FILENAME="${MYSQL_DATABASE}_$(date +"%Y-%m-%d_%H-%M").sql.gz"
-                                echo "Dumping database $MYSQL_DATABASE to $BACKUP_PATH/$BACKUP_FILENAME."
-                                echo "BEGIN DUMP " $(date) >>$script_path/logs-backup.log
-                                MYSQL_PWD="${MYSQL_PASSWORD}" mysqldump -u "${MYSQL_USERNAME}" -h "${MYSQL_HOST}" --single-transaction --databases "${MYSQL_DATABASE}" 2>/dev/null | gzip -9 >$BACKUP_PATH/$BACKUP_FILENAME
 
-                                if [ $? -eq 0 ]; then
-                                    echo "END DUMP " $(date) >>$script_path/logs-backup.log
-                                    echo "Dumping database successful. Backing up to remote host $REMOTE_HOST as user $REMOTE_USER."
-                                    echo "BEGIN FILE TRANSFER " $(date) >>$script_path/logs-backup.log
-                                    scp $BACKUP_PATH/$BACKUP_FILENAME $REMOTE_USER@$REMOTE_HOST:$REMOTE_PATH
+                                if [ "$dry_run" = true ]; then
+                                    if [[ "$LOCAL_BACKUPS" = true ]]; then
+                                        echo "Local backups are enabled."
+
+                                        if [[ "$BACKUP_EXPIRES" -ne -1 ]]; then
+                                            echo "Local backups expire after $BACKUP_EXPIRES days."
+                                        else
+                                            echo "Local backups set to not expire."
+                                        fi
+                                    fi
+                                    echo "Connection test successful."
+                                    return 0
+                                else
+                                    BACKUP_FILENAME="${MYSQL_DATABASE}_$(date +"%Y-%m-%d_%H-%M").sql.gz"
+                                    echo "Dumping database $MYSQL_DATABASE to $BACKUP_PATH/$BACKUP_FILENAME."
+                                    log_message "BEGIN DUMP"
+                                    MYSQL_PWD="${MYSQL_PASSWORD}" mysqldump -u "${MYSQL_USERNAME}" -h "${MYSQL_HOST}" --single-transaction --databases "${MYSQL_DATABASE}" 2>/dev/null | gzip -9 >$BACKUP_PATH/$BACKUP_FILENAME
 
                                     if [ $? -eq 0 ]; then
-                                        echo "END FILE TRANSFER " $(date) >>$script_path/logs-backup.log
+                                        log_message "END DUMP"
+                                        echo "Dumping database successful. Backing up to remote host $REMOTE_HOST as user $REMOTE_USER."
+                                        log_message "BEGIN FILE TRANSFER "
+                                        scp $BACKUP_PATH/$BACKUP_FILENAME $REMOTE_USER@$REMOTE_HOST:$REMOTE_PATH
 
-                                        if [ "$LOCAL_BACKUPS" = false ]; then
-                                            echo "Backup to remote host successful, deleting temporary file."
-                                            echo "DELETING LOCAL BACKUP " $(date) >>$script_path/logs-backup.log
-                                            rm $BACKUP_PATH/$BACKUP_FILENAME
+                                        if [ $? -eq 0 ]; then
+                                            log_message "END FILE TRANSFER"
 
-                                            if [ $? -eq 0 ]; then
-                                                echo "Local backup deleted, exiting."
-                                                echo "LOCAL BACKUP DELETED " $(date) >>$script_path/logs-backup.log
-                                                return 0
+                                            if [ "$LOCAL_BACKUPS" = false ]; then
+                                                echo "Backup to remote host successful, deleting temporary file."
+                                                log_message "DELETING LOCAL BACKUP"
+                                                rm $BACKUP_PATH/$BACKUP_FILENAME
+
+                                                if [ $? -eq 0 ]; then
+                                                    echo "Local backup deleted."
+                                                    log_message "LOCAL BACKUP DELETED"
+                                                    return 0
+                                                else
+                                                    echo "Local backup deleting failed."
+                                                    log_message "DELETING LOCAL BACKUP FAILED"
+                                                    return 1
+                                                fi
                                             else
-                                                echo "Local backup deleting failed, exiting."
-                                                echo "DELETING LOCAL BACKUP FAILED " $(date) >>$script_path/logs-backup.log
-                                                return 1
+                                                echo "Backup to remote host successful."
+                                                return 0
                                             fi
                                         else
-                                            echo "Backup to remote host successful, exiting."
-                                            return 0
+                                            echo "Backup to remote host failed." >&2
+                                            log_message "FILE TRANSFER FAILED"
+                                            return 1
                                         fi
                                     else
-                                        echo "Backup to remote host failed, exiting." >&2
-                                        echo "FILE TRANSFER FAILED " $(date) >>$script_path/logs-backup.log
+                                        echo "Dumping database failed." >&2
+                                        log_message "DUMP FAILED"
                                         return 1
                                     fi
-                                else
-                                    echo "Dumping database failed, exiting." >&2
-                                    echo "DUMP FAILED " $(date) >>$script_path/logs-backup.log
-                                    return 1
                                 fi
                             else
-                                echo "User does NOT have required privileges, exiting." >&2
-                                echo "MYSQL USER BAD PERMS " $(date) >>$script_path/logs-backup.log
+                                echo "User does NOT have required privileges." >&2
+                                log_message "MYSQL USER BAD PERMS"
                                 return 1
                             fi
                         else
-                            echo "Database is NOT usable, exiting." >&2
-                            echo "MYSQL DB NOT USABLE " $(date) >>$script_path/logs-backup.log
+                            echo "Database is NOT usable." >&2
+                            log_message "MYSQL DB NOT USABLE"
                             return 1
                         fi
                     else
-                        echo "Login to MySQL failed, exiting." >&2
-                        echo "MYSQL BAD LOGIN " $(date) >>$script_path/logs-backup.log
+                        echo "Login to MySQL failed." >&2
+                        log_message "MYSQL BAD LOGIN"
                         return 1
                     fi
                 else
-                    echo "Writing to remote path failed, exiting." >&2
-                    echo "REMOTE PATH NO PERMS " $(date) >>$script_path/logs-backup.log
+                    echo "Writing to remote path failed." >&2
+                    log_message "REMOTE PATH NO PERMS"
                     return 1
                 fi
             else
-                echo "Login to remote host failed, exiting." >&2
-                echo "REMOTE HOST BAD LOGIN " $(date) >>$script_path/logs-backup.log
+                echo "Login to remote host failed." >&2
+                log_message "REMOTE HOST BAD LOGIN"
                 return 1
             fi
         else
-            echo "The database backup directory is not writable, exiting." >&2
-            echo "LOCAL BACKUP DIR NO PERMS " $(date) >>$script_path/logs-backup.log
+            echo "The database backup directory is not writable." >&2
+            log_message "LOCAL BACKUP DIR NO PERMS"
             return 1
         fi
     else
-        echo "The script directory is not writable, exiting." >&2
-        echo "SCRIPT DIR NO PERMS " $(date) >>$script_path/logs-backup.log
+        echo "The script directory is not writable." >&2
+        log_message "SCRIPT DIR NO PERMS"
         return 1
     fi
 }
 
 delete_backups() {
-    CURRENT_DATE=$(date +%s)
-    for file in "$BACKUP_PATH"/*.sql.gz; do
-        if [[ $file =~ _([0-9]{4}-[0-9]{2}-[0-9]{2})_[0-9]{2}-[0-9]{2}\.sql\.gz ]]; then
-            FILE_DATE=${BASH_REMATCH[1]}
-            FILE_DATE_EPOCH=$(date -d "$FILE_DATE" +%s)
-            FILE_AGE=$(( (CURRENT_DATE - FILE_DATE_EPOCH) / (60*60*24) ))
-            if (( FILE_AGE > BACKUP_EXPIRES )); then
-                if [ "$dry_run" = true ]; then
-                    echo "$file (age: $FILE_AGE days) is older than (BACKUP_EXPIRES: $BACKUP_EXPIRES days)"
-                else
-                    echo "Deleting $file (age: $FILE_AGE days)"
-                    echo "DELETING EXPIRED BACKUP $file " $(date) >>$script_path/logs-backup.log
-                    rm -f "$file"
+    if [ "$BACKUP_EXPIRES" -ne -1 ] && [ "$LOCAL_BACKUPS" = true ]; then
+        echo "Checking for expired local backups."
+        log_message "CHECKING FOR EXPIRED LOCAL BACKUPS"
+        CURRENT_DATE=$(date +%s)
+        for file in "$BACKUP_PATH"/*.sql.gz; do
+            if [[ $file =~ _([0-9]{4}-[0-9]{2}-[0-9]{2})_[0-9]{2}-[0-9]{2}\.sql\.gz ]]; then
+                FILE_DATE=${BASH_REMATCH[1]}
+                FILE_DATE_EPOCH=$(date -d "$FILE_DATE" +%s)
+                FILE_AGE=$(( (CURRENT_DATE - FILE_DATE_EPOCH) / (60*60*24) ))
+                if (( FILE_AGE > BACKUP_EXPIRES )); then
+                    if [ "$dry_run" = true ]; then
+                        echo "$file (age: $FILE_AGE days) is older than (BACKUP_EXPIRES: $BACKUP_EXPIRES days)"
+                    else
+                        echo "Deleting $file (age: $FILE_AGE days)"
+                        log_message "DELETING EXPIRED BACKUP $file"
+                        rm -f "$file"
+                    fi
                 fi
             fi
-        fi
-    done
-    return 0
+        done
+        return 0
+    else
+        return 1
+    fi
 }
 
 print_help() {
@@ -235,6 +201,8 @@ unset_variables() {
     unset SLACK_WEBHOOK_URL
 }
 
+dry_run=false
+
 while [[ "$#" -gt 0 ]]; do
     case $1 in
         -c | -config)
@@ -258,14 +226,14 @@ while [[ "$#" -gt 0 ]]; do
 done
 
 if [ -z "$config_path" ]; then
-    echo "Config path or file not specified, exiting."
+    echo "Config path or file not specified."
     exit 1
 fi
 
 if [ -d "$config_path" ]; then
     config_files=("$config_path"/*.conf)
     if [ -z "$(ls -A "$config_path")" ]; then
-        echo "Config directory is empty, exiting."
+        echo "Config directory is empty."
         exit 1
     fi
 else
@@ -273,7 +241,7 @@ else
 fi
 
 if [ ${#config_files[@]} -eq 0 ]; then
-    echo "No config files found, exiting."
+    echo "No config files found."
     exit 1
 fi
 
@@ -282,46 +250,21 @@ for config_file in "${config_files[@]}"; do
         unset_variables
         source "$config_file"
         if [ "$MYSQL_BCKTOOL_CFG_VER" = $version ]; then
-            if [ "$dry_run" = true ]; then
-                dry_run
-                if [ $? -ne 0 ]; then
-                    ERROR=true
-                else
-                    if [ "$BACKUP_EXPIRES" -ne -1 ] && [ "$LOCAL_BACKUPS" = true ]; then
-                        echo "Checking for expired local backups."
-                        delete_backups
-                    fi
-                fi
+            run_backup            
+            if [ $? -ne 0 ]; then
+                ERROR=true
+                slack_message "Database backup failed! :face_with_head_bandage:" "$config_file" "Failed" "#d33f3f"
             else
-                live_run
-                
-                if [ $? -ne 0 ]; then
-                    ERROR=true
-                    if [ "$SLACK_INTEGRATION" = true ]; then
-                        slack_message "Database backup failed! :face_with_head_bandage:" "$config_file" "Failed" "#d33f3f"
-                    fi
-                else
-                    if [ "$BACKUP_EXPIRES" -ne -1 ] && [ "$LOCAL_BACKUPS" = true ]; then
-                        echo "Checking for expired local backups."
-                        echo "CHECKING FOR EXPIRED LOCAL BACKUPS" $(date) >>$script_path/logs-backup.log
-                        delete_backups
-                    fi
-                    if [ "$SLACK_INTEGRATION" = true ]; then
-                        slack_message "Database backup completed! :tada:" "$config_file" "Completed" "#61d33f"
-                    fi
-                fi
+                delete_backups
+                slack_message "Database backup completed! :tada:" "$config_file" "Completed" "#61d33f"
             fi
         else
-            if [ "$dry_run" = false ]; then
-                echo "Config file $config_file $MYSQL_BCKTOOL_CFG_VER version mismatch, expected $version, skipping."
-                echo "CONFIG FILE $config_file $MYSQL_BCKTOOL_CFG_VER VERSION MISMATCH " $(date) >>$script_path/logs-backup.log
-            fi
+            echo "Config file $config_file $MYSQL_BCKTOOL_CFG_VER version mismatch, expected $version, skipping."
+            log_message "CONFIG FILE $config_file $MYSQL_BCKTOOL_CFG_VER VERSION MISMATCH"
         fi
-        echo "---------------------------------" >>$script_path/logs-backup.log
+        log_message "---------------------------------"
     else
-        if [ "$dry_run" = false ]; then
-            echo "Config file $config_file missing, skipping."
-        fi
+        echo "Config file $config_file missing, skipping."
     fi
 done
 
